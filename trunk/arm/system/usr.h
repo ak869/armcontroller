cd3b7c827fc6913773267fb6c20402f11d3b7f55
leon.b.dong <leon.b.dong@41cf9b94-d0c3-11dd-86c2-0f8696b7b6f9>
2008-12-25 02:31:49 +0000

    上个版本漏传了usr.h文件

---
+++ trunk/arm/system/usr.h
@@ -0,0 +1,13 @@
+#ifndef _USR_H
+#define _USR_H
+
+#define SAFE_TYPE		0x00
+#define OPEN_TYPE		0x01
+#define CLOSE_TYPE		0x02
+#define NONE_TYPE		0xff
+
+#define POWERID_TYPE				0x40
+#define REPEAT_TYPE					0x10
+#define NOPOWERID_TYPE				0x80
+#define ALARM_TYPE					0x20
+#endif
